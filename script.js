bfd3b6ae31a34cd61f4ecd0014b68a350c07fd7a
YanellaFT <yanellaft@gmail.com>
2025-07-20 22:55:07 +0000

    setup audio

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+//variables
+let mic;
+let fft;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
+
+  //set up microphone input
+  mic = new p5.AudioIn();
+  mic.start()
+
+  //set up fast fourier transform (fft) algorithm
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
 function draw() {
